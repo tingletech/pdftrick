301dacccdeff0dbeca4e12a5f8d6ea8dc769bfe9
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2014-02-22 10:45:12 -0800

    set up for pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup, find_packages
 setup(
     name='pdftrick',
-    version = "0.0.0",
+    version = "0.0.1",
+    description = "One weird PDF trick",
+    url='https://github.com/tingletech/pdftrick',
+    download_url = 'https://github.com/tingletech/pdftrick/tarball/0.0.1',
+    author="Brian Tingle",
+    author_email="brian.tingle.cdlib.org@gmail.com",
+    license = "BSD",
+    keywords = "PDF compression",
     packages = find_packages(),
     entry_points={
         'console_scripts': [
