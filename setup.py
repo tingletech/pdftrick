ddf6578314c69d28929cd24cb3ff7fe7059a27be
Brian Tingle <brian.tingle@ucop.edu>
2018-07-25 22:00:55 -0700

    setuptools tweaks
    fix entry_points
    get subprocess32 backport
    bump version

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from setuptools import setup, find_packages
 setup(
     name='pdftrick',
-    version = "0.0.3",
+    version = "0.0.4",
     description = "One weird PDF trick",
     url='https://github.com/tingletech/pdftrick',
-    download_url = 'https://github.com/tingletech/pdftrick/tarball/0.0.3',
+    download_url = 'https://github.com/tingletech/pdftrick/tarball/0.0.4',
     author="Brian Tingle",
     author_email="brian.tingle.cdlib.org@gmail.com",
     license = "BSD",
     keywords = "PDF compression",
     packages = find_packages(),
+    install_requires = [
+        'subprocess32',
+    ],
     entry_points={
         'console_scripts': [
-            'pdftrick = pdftrick.pdftrick:main'
-            'pdfdensity = pdftrick.pdfdensity:main'
+            'pdftrick=pdftrick.pdftrick:main',
+            'pdfdensity=pdftrick.pdfdensity:main',
         ]
     }
 )
